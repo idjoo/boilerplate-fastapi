787a3eb13857e2d0fc53073a4b15ada265fbbd9c
Adrianus Vian Habirowo <adrianus.vian.habirowo@devoteam.com>
2025-11-28 17:09:46 +0700

    fix: ignore linting for Generic response

--- src/schemas/response_schema.py
+++ src/schemas/response_schema.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 T = TypeVar("T")
 
 
-class Response(BaseModel, Generic[T]):
+class Response(BaseModel, Generic[T]):  # noqa: UP046
     status: int = 200
     message: str = ""
     data: T | None
